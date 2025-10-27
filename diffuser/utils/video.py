445e7dc67e1c0226d8359eea4a4ed2e0b4710fbe
Jinxin Liu <liujinxin1996@gmail.com>
2025-10-27 20:46:45 +0800

    modify

--- diffuser/utils/video.py
+++ diffuser/utils/video.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
-import skvideo.io
+import imageio
 
 def _make_dir(filename):
     folder = os.path.dirname(filename)
@@ -11,17 +11,18 @@ def save_video(filename, video_frames, fps=60, video_format='mp4'):
     assert fps == int(fps), fps
     _make_dir(filename)
 
-    skvideo.io.vwrite(
-        filename,
-        video_frames,
-        inputdict={
-            '-r': str(int(fps)),
-        },
-        outputdict={
-            '-f': video_format,
-            '-pix_fmt': 'yuv420p', # '-pix_fmt=yuv420p' needed for osx https://github.com/scikit-video/scikit-video/issues/74
-        }
-    )
+    # Ensure video_frames is uint8 and in RGB format
+    video_frames = np.asarray(video_frames)
+    if video_frames.dtype != np.uint8:
+        video_frames = (255 * np.clip(video_frames, 0, 1)).astype(np.uint8)
+    if video_frames.shape[-1] == 3:
+        # If frames are BGR (from OpenCV), convert to RGB
+        # Assume input is RGB, if not, user should convert before
+        pass
+
+    with imageio.get_writer(filename, fps=int(fps), format=video_format) as writer:
+        for frame in video_frames:
+            writer.append_data(frame)
 
 def save_videos(filename, *video_frames, axis=1, **kwargs):
     ## video_frame : [ N x H x W x C ]
